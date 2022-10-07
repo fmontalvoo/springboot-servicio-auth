ee3c4c95f8df0e372784061cd0e05c96707dfa44
Frank Montalvo Ochoa <fgmo.dev@fmontalvoo.com>
2022-10-06 19:48:27 -0500

    Initial commit
    Signed-off-by: Frank Montalvo Ochoa <fgmo.dev@fmontalvoo.com>

---
+++ src/main/java/com/fmontalvoo/springboot/app/auth/SpringbootServicioAuthApplication.java
@@ -0,0 +1,13 @@
+package com.fmontalvoo.springboot.app.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmontalvoo/springboot/app/auth/SpringbootServicioAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmontalvoo.springboot.app.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
